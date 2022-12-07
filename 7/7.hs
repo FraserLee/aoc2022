c2505b46d6434cdf9709b0cb046679c20b5f7a9d
Fraser <tomatoesneverlie@gmail.com>
2022-12-07 15:58:46 -0500

    cleaned up a bunch

--- 7/7.hs
+++ 7/7.hs
@@ -1,81 +1,58 @@
 -- stack script --resolver lts-20.3 --package regex-pcre
 import Data.List
 import Data.Maybe
-import Text.Regex.PCRE
-import Debug.Trace
 
 data Dir = Dir { files :: [(String, Int)], dirs :: [(String, Dir)] }
-
-instance Show Dir where
-    show d = show' d ""
-        where show' (Dir fs ds) prefix = unlines 
-                $ map (\(name, size) -> prefix ++ "file-" ++ name ++ "-" ++ show size) fs
-               ++ map (\(name, dir) -> prefix ++ "dir-" ++ name ++ "\n" ++ show' dir (prefix ++ "  ")) ds
-
 data Action = CD String | Create String
-instance Show Action where
-    show (CD s) = "CD " ++ s
-    show (Create s) = "Create " ++ s
 
-pop :: Eq a => a -> [(a, b)] -> (b, [(a, b)])
-pop _ [] = error "key not found"
-pop key ((c_key, c_val):cs) = case key == c_key of
-    True -> (c_val, cs)
-    False -> let (val, cs') = pop key cs in (val, (c_key, c_val):cs')
+-- two mutually recursive functions, that hand control back and forth to represent context.
+parse_prompt, parse_ls :: [String] -> [Action]
+parse_prompt [] = []
+parse_prompt ("$ ls":lines) = parse_ls lines
+parse_prompt (cd:lines) = CD (last $ words cd) : parse_prompt lines
 
--- act should still return the root dir, even after a cd
+parse_ls [] = []
+parse_ls lines@(('$':_):_) = parse_prompt lines
+parse_ls (l:lines) = Create l : parse_ls lines
+
+-- add file or directory to Dir
+add :: Dir -> String -> Dir
+add dir ('d':'i':'r':' ':name) = dir { dirs = (name, Dir [] []) : dirs dir }
+add dir file = let size:name:_ = words file
+               in dir { files = (name, read size) : files dir }
+
+-- dir, list of actions -> possibly modified dir, probably empty list of remaining actions.
 act :: Dir -> [Action] -> (Dir, [Action])
 act dir [] = (dir, [])
-act dir (CD name:as) =
-    if name == ".." then (dir, as)
-    else let (sub_d, dirs') = pop name (dirs dir)
-             (sub_d', as') = act sub_d as
-         in act (Dir (files dir) ((name, sub_d'):dirs')) as'
-
 act dir (Create l:as) = act (add dir l) as
+act dir (CD "..":as) = (dir, as)
+act dir (CD name:as) =
 
+    let pop key ((c_key, c_val):cs) = case key == c_key of
+            True -> (c_val, cs)
+            False -> let (val, cs') = pop key cs in (val, (c_key, c_val):cs')
 
-add :: Dir -> String -> Dir
-add dir line = case line of
-    ('d':'i':'r':' ':sub_d) -> 
-        dir { dirs = (sub_d, Dir [] []):dirs dir }
-    _ -> 
-        let size':name:_ = words line 
-            size = read size' :: Int
-        in dir { files = (name, size):files dir }
+        (sub_d, dirs') = pop name (dirs dir)
+        (sub_d', as') = act sub_d as
 
-parse_cmd :: [String] -> [Action]
-parse_cmd [] = []
-parse_cmd (l:lines) = case l of
-    "$ ls" -> parse_ls_output lines
-    _ -> CD (last $ words l) : parse_cmd lines
+    in act (Dir (files dir) ((name, sub_d'):dirs')) as'
 
-parse_ls_output :: [String] -> [Action]
-parse_ls_output [] = []
-parse_ls_output (l:lines) = case l of
-    '$':_ -> parse_cmd (l:lines)
-    _ -> Create l : parse_ls_output lines
 
 size :: Dir -> Int
 size (Dir fs ds) = sum (map snd fs) + sum (map (size . snd) ds)
 
 -- traverse every directory with an accumulator
 fold :: (a -> Dir -> a) -> a -> Dir -> a
-fold f acc (Dir fs ds) = 
-    let acc' = f acc (Dir fs ds)
-    in foldl' (fold f) acc' (map snd ds)
+fold f acc (Dir fs ds) = let acc' = f acc (Dir fs ds)
+                         in foldl' (fold f) acc' (map snd ds)
 
 main = do
+
     file <- readFile "input.txt"
 
-    let (root, _) = act (Dir [] []) $ parse_cmd $ tail $ lines file
-    let total_bounded = fold (\acc d -> if size d <= 100000 then acc + size d else acc) 0 root
+    let (root, _) = act (Dir [] []) $ parse_prompt $ tail $ lines file
+    let total_bounded = fold (\acc d -> let s = size d in if s <= 100000 then acc + s else acc) 0 root
     let needed_space = 30000000 - (70000000 - size root)
-    let min_greater = fold (\m d -> 
-                -- trace (show $ m > size d) $ trace (show $ size d >= needed_space) $ 
-                if (m > size d) && (size d >= needed_space) then size d else m) 0x7fffffff root
+    let min_greater = fold (\m d -> let s = size d in if (m > s) && (s >= needed_space) then s else m) 0x7fffffff root
 
-    print $ (total_bounded, min_greater)
-
-
-
+    print (total_bounded, min_greater)
